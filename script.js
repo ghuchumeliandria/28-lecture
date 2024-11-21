725802e10f0db975b98149976471e25714d09037
Andria Ghurtchumelia <andriaghurchumelia4@gmail.com>
2024-11-21 18:04:10 +0400

    js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 
 let button = document.getElementById("button");
 
-let boolen = true;
 
 
 button.addEventListener("click", () => {
-  localStorage.setItem('key' , JSON.stringify(boolen))
-  
-  let getItem = JSON.parse(localStorage.getItem("key"))
-  if(getItem == true){
-    document.body.style.backgroundColor = "black";
-    boolen = false
+  let setTheme = document.body
+
+  setTheme.classList.toggle("dark-mode")
+  let theme;
+  if(setTheme.classList.contains("dark-mode")){
+    console.log("dark-mode")
+    theme = "DARK"
   }else{
-    document.body.style.backgroundColor = "white";
-    boolen = true;
+    theme = "WHITE"
+    console.log("white")
+    localStorage.setItem("PageMode" , JSON.stringify(theme))
   }
 });
+let getItem = JSON.parse(localStorage.getItem("PageMode"))
+console.log(getItem)
+// let getItem = JSON.parse(localStorage.getItem("key"))
+// console.log(getItem)
+// if(getItem == "DARK"){
+//     document.body.style.backgroundColor = "black"
+
+if(getItem === "DARK"){
+  document.body.classList = "dark-mode"
+}
